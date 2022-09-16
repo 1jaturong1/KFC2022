2bcd1a9be5d13272faa1b1c9669c5ec273a484ce
Jeep <78637075+1jaturong1@users.noreply.github.com>
2022-09-16 12:02:06 +0700

    Add files via upload

---
+++ 091622/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _091622
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Capital =  1000000;
+            string branchname;
+            string address;
+            int Cost;
+            int CD;
+        
+       Console.WriteLine("You have CapitalOpen To KFC :{0}Capital");
+       Console.WrtieLine("BranchName : {0}branchname");
+       Console.WrtieLine("NameAddress :{0}address");
+        }
+        public void StoreRegistration()//ลงทะเบียนร้าน
+        {
+        public static string InputName() {
+        Console.Write("Input Name: ");
+
+        return Console.ReadLine();
+    }
+        public static string InputSurname() {
+        Console.Write("Input Surname: ");
+
+        return Console.ReadLine();
+    }
+        public static string InputIDname() {
+        Console.Write("Input Idname: ");
+
+        return Console.ReadLine();
+    }
+
+        
+
+        public void WelfareBenefitKFC()//คำนวณสวัสดิการร้าน
+        {
+            CostTravel = days*275;
+            Share = Cost*2.5*100;
+            CostDelivery = CD * 150;
+        }
+
+    static void Main(string[] args)
+        {
+            int Capital =  1000000;
+            string branchname;
+            string address;
+            int Cost;
+            int CD;
+        }
+
+
+    }
+    class Employee{
+
+     public void Employee()//ผนักงาน
+        {
+            public void Shop()//ฝ่ายขาย
+            {
+            string Name;
+            string Surname;
+            string Age;
+            int Salary = 15000;
+
+            public void WelfareBenefitShop()//สวัสดิการขาย
+            {
+            int CostTravel;
+            int Share;
+            }
+            }
+             public void Delivery()//ฝ่ายจัดส่ง
+            {
+            string Name;
+            string Surname;
+            string Age;
+            int Salary = 12500;
+
+            public void WelfareBenefitDelivery()//สวัสดิการจัดส่ง
+            {
+            int CostTravel;
+            int Share;
+            int CostDelivery;
+            }
+            }
+        }
+    }
+
+
+
+}
